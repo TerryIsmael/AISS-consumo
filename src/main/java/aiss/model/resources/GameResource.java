b04fabbc37f122852badaab2db65db5a95731b13
Daniel <danarrarr@alum.us.es>
2022-05-27 23:47:53 +0200

    Added post, put and delete game

--- src/main/java/aiss/model/resources/GameResource.java
+++ src/main/java/aiss/model/resources/GameResource.java
@@ -5,13 +5,14 @@ import java.net.URLEncoder;
 import java.util.function.Predicate;
 
 import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
 
 import aiss.model.game.Game;
 
 public class GameResource {
 	
 	Predicate<String> isNull = v->v==null || v.equals("") ;
-
+	String uri = "https://indigo-computer-349516.ew.r.appspot.com/api/games";
 	public Game[] getGames (String order, String order2, String q, Integer limit, Integer offset) throws UnsupportedEncodingException {
 		String orderE = null;
 		String order2E = null;
@@ -31,5 +32,56 @@ public class GameResource {
 		
 		return games;
 	}
-	
+	public Game getGame (String id) {
+		ClientResource cr = null;
+		Game game = null;
+		try {
+			cr = new ClientResource(uri +"/" + id);
+			
+			game = cr.get(Game.class);
+		} catch (ResourceException e){
+			System.err.println("Error when getting a game " + cr.getResponse().getStatus());
+			throw e;
+		}
+		return game;
+	}
+	public Game addGame(Game game) {
+		ClientResource cr = null;
+		Game result = null;
+		try {
+			cr = new ClientResource(uri);
+			cr.setEntityBuffering(true);
+			result = cr.post(game, Game.class);
+		} catch (ResourceException e) {
+			System.err.println("Error when adding a game " + cr.getResponse().getStatus());
+			throw e;
+		}
+		return result;
+	}
+	public boolean updateGame(Game game) {
+		ClientResource cr = null;
+		Boolean success = true;
+		try {
+			cr = new ClientResource(uri);
+			cr.setEntityBuffering(true);
+			cr.put(game);
+		} catch (ResourceException e) {
+			System.err.println("Error when updating a game " + cr.getResponse().getStatus());
+			success = false;
+		}
+		return success;
+	}
+	public boolean deleteGame(String gameId) {
+		ClientResource cr = null;
+		Boolean success = true;
+		try {
+			cr= new ClientResource(uri + "/" + gameId);
+			cr.setEntityBuffering(true);
+			cr.delete();
+		} catch (ResourceException e) {
+			System.err.println("Error when deleting a game " + cr.getResponse().getStatus());
+			success=false;
+		}
+		return success;
+	}
 }
--- src/test/java/aiss/model/resources/GameTest.java
+++ src/test/java/aiss/model/resources/GameTest.java
@@ -17,17 +17,20 @@ public class GameTest {
 		String title = "star wars";
 		GameResource gameR = new GameResource();
 		Game[] gameResults = gameR.getGames(null, null, title, null, null);
-		
+		String id = "G1869";
+		Game gameResult = gameR.getGame(id);
 		
 		assertNotNull("The search returned null", gameResults);
 		assertFalse("The number of albums of " + title + " is zero", gameResults.length==0);
 		
 		System.out.println("The search for " + title + "'s title returned " +  gameResults.length + " games.");
 		
+		
 		//Print movies data
 		for(Game game : gameResults) {
 			System.out.println("Game: " + game.getName());
 		}
+		System.out.println(String.format("The search for game which id is %s is %s", id, gameResult.getName()));
 	}
 	
 }
